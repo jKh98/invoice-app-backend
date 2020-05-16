71b8e35f733b8e2cbd84c0ff22d62508e3a1400d
jihad khurfan <jkhurfan.jk@gmail.com>
2020-05-16 18:11:54 +0300

    added customer schema

---
+++ models/customer.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const CustomerSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 4
+    },
+    company: {
+        type: String,
+        trim: true,
+        required: false,
+    },
+    email: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true,
+        minlength: 7
+    },
+    phone: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 8
+    },
+    mobile: {
+        type: String,
+        trim: true,
+        required: false,
+        minlength: 8
+    },
+    merchant: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    addresses: [{
+        type: String,
+        required: true,
+    }]
+})
+
+CustomerSchema.methods.toJSON = function () {
+    const customer = this;
+    const customerObject = customer.toObject();
+    return _.pick(customerObject, ["_id", "name", "email","company","phone","mobile","addresses","merchant"]);
+}
+
+
+const Customer = mongoose.model('Customer', CustomerSchema, 'customers')
+
+module.exports = {Customer};
