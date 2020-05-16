2ee712c31260f9de2386d7b0142683ef75a3bb39
jihad khurfan <jkhurfan.jk@gmail.com>
2020-05-16 23:01:01 +0300

    added functionality of getting all customers

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -6,7 +6,6 @@ const authenticate = require("../middlewares/authenticate")
 
 
 router.post("/edit", authenticate, (req, res) => {
-
     const query = {
         email: req.body.email,
         merchant: req.user
@@ -23,7 +22,6 @@ router.post("/edit", authenticate, (req, res) => {
     }
     const options = {upsert: true, new: true, useFindAndModify: false};
     Customer.findOneAndUpdate(query, customerData, options).then((customer) => {
-        console.log(customer)
         if (customer) {
             res.send(customer);
         } else {
@@ -32,19 +30,22 @@ router.post("/edit", authenticate, (req, res) => {
     }).catch((error) => {
         res.status(500).send(error);
     });
-
-
-    // const user = new User(userData)
-    // user.save().then(() => {
-    //     if (user) {
-    //         return user.generateAuthToken()
-    //     } else
-    //         res.sendStatus(400)
-    // }).then((token) => {
-    //     res.header({"x-auth": token}).send(user)
-    // }).catch((e) => {
-    //     res.status(400).send(e);
-    // })
 });
+
+router.get("/all", authenticate, (req, res) => {
+    const query = {
+        merchant: req.user
+    }
+    Customer.find(query).then((customers) => {
+        console.log(customers)
+        if (customers) {
+            res.send(customers);
+        } else {
+            res.status(500).send("No data available");
+        }
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+})
 
 module.exports = router;
